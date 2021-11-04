6a6f102bb6f8eacc08aba49acc218741ce84b3e2
EC2 Default User <ec2-user@ip-172-31-59-71.ec2.internal>
2021-11-04 02:25:25 +0000

    1st commit

--- app/controllers/end_user/sessions_controller.rb
+++ app/controllers/end_user/sessions_controller.rb
@@ -37,5 +37,5 @@ class EndUser::SessionsController < Devise::SessionsController
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  # endg
 end
