3ae423e80da79c1dc782ae4cc52ea65ba700b16f
shunsukeharada <ec2-user@ip-172-31-59-71.ec2.internal>
2021-11-07 03:21:28 +0000

    1st commit

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -16,4 +16,19 @@ class EndUser < ApplicationRecord
   validates :postal_code, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
+  
+  def update_with_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank?
+        params.delete(:password)
+        params.delete(:password_confirmation) if params[:password_confirmation].blank?
+    end
+
+    result = update(params, *options)
+
+    clean_up_passwords
+    result
+  end
+  
 end
