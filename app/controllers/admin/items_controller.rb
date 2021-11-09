a09629b058bfa197ebe30149fc3b7d4aa101b4be
shunsukeharada <ec2-user@ip-172-31-59-71.ec2.internal>
2021-11-09 07:59:36 +0000

    1st commit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,23 @@
 class Admin::ItemsController < ApplicationController
+    def new
+        @item = Item.new
+    end
+    
+    def index
+        @items = Item.all
+    end
+    
+    def show
+        @item = Item.find(params[:id])
+    end
+    
+    def create
+        @item = Item.new(item_params)
+        @item.save!
+        redirect_to admin_item_path(@item)
+    end
+    
+    def item_params
+        params.require(:item).permit(:image, :name, :explanation, :genre_id, :tax_out_price, :is_sale)
+    end
 end
