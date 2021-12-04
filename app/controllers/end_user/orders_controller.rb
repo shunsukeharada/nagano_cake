14a1b96732076191f91ae3ccb82df35b9aeca2ca
shunsukeharada <ec2-user@ip-172-31-59-71.ec2.internal>
2021-12-04 05:10:07 +0000

    1st commit

--- app/controllers/end_user/orders_controller.rb
+++ app/controllers/end_user/orders_controller.rb
@@ -1,2 +1,59 @@
 class EndUser::OrdersController < ApplicationController
+    before_action :check_cart_items, only: [:new, :create]
+    
+    def new
+        @order = Order.new
+    end
+    
+    def confirm
+        @order = Order.new(order_params)
+        if  params[:order][:select_address] == "0"
+            @order.name = current_end_user.first_name + current_end_user.last_name
+            @order.postal_code = current_end_user.postal_code
+            @order.address = current_end_user.address
+        elsif params[:order][:select_address] == "1"
+                selected_address = Address.find(params[:order][:address_id])
+                @order.postal_code = selected_address.postal_code
+                @order.name = selected_address.name
+                @order.address = selected_address.address
+        elsif params[:order][:select_address] == "2"
+            address_new = current_end_user.addresses.new(address_params)
+            address_new.save
+        end
+        @cart_items = current_end_user.cart_items.all
+        @total = @cart_items.inject(0) { |sum, cart_item| sum + cart_item.subtotal }
+    end
+    
+    def create
+        @cart_items = current_end_user.cart_items.all
+        total_price = @cart_items.inject(0) { |sum, cart_item| sum + cart_item.subtotal }
+        @order = current_end_user.orders.new(order_params)
+        @cart_items.each do |cart_item|
+            @order_detail = @order.order_details.new
+            @order_detail.item_id = cart_item.item_id
+            @order_detail.quantity = cart_item.amount
+            @order_detail.price = total_price
+            @order_detail.save
+        end
+        @cart_items.destroy_all
+        @order.save
+        redirect_to orders_complete_path
+    end
+    
+    private
+    
+    def order_params
+        params.require(:order).permit(:name, :postal_code, :address, :payment_method)
+    end
+    
+    def address_params
+        params.require(:order).permit(:name, :postal_code, :address)
+    end
+    
+    def check_cart_items
+        if current_end_user.cart_items.blank?
+          redirect_to root_path
+        end
+    end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,14 @@
 class Order < ApplicationRecord
-  belongs_to :end_users
+  belongs_to :end_user
   has_many :order_details, dependent: :destroy
+  
+  
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum order_status: {
+    waiting: 0,
+    paid_up: 1,
+    producing: 2,
+    preparing: 3,
+    shipped: 4
+  }
 end
